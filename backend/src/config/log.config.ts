cc4b46d4c41b6e23bcf8f10314cef887de774196
Kuroko <dromarks@outlook.com>
2018-02-23 17:53:00 +0800

    完善log4js的输出部分

--- backend/src/config/log.config.ts
+++ backend/src/config/log.config.ts
@@ -28,38 +28,86 @@ const responseFileName = "response";
 //响应日志输出完整路径
 const responseLogPath = baseLogPath + responsePath + "/" + responseFileName;
 
+// export default {
+//     "appenders": [
+//         // 错误日志
+//         {
+//             // 日志类型          
+//             "type": "dateFile",
+//             // logger名称
+//             "category": "errorLogger",
+//             // 日志输出位置                 
+//             "filename": errorLogPath,
+//             // 是否总是有后缀名          
+//             "alwaysIncludePattern": true,
+//             // 后缀，每小时创建一个新的日志文件        
+//             "pattern": "-yyyy-MM-dd-hh.log",
+//             // 自定义属性，错误日志的根目录  
+//             "path": errorPath
+//         },
+//         // 响应日志
+//         {
+//             "type": "dateFile",
+//             "category": "resLogger",
+//             "filename": responseLogPath,
+//             "alwaysIncludePattern": true,
+//             "pattern": "-yyyy-MM-dd-hh.log",
+//             "path": responsePath
+//         }
+//     ],
+//     // 设置logger名称对应的的日志等级
+//     "levels": {
+//         "errorLogger": "ERROR",
+//         "resLogger": "ALL"
+//     },
+//     // logs根目录
+//     "baseLogPath": baseLogPath
+// }
 export default {
-    "appenders": [
-        // 错误日志
-        {
-            // 日志类型          
+    "appenders": {
+        "access": {
             "type": "dateFile",
-            // logger名称
-            "category": "errorLogger",
-            // 日志输出位置                 
-            "filename": errorLogPath,
-            // 是否总是有后缀名          
-            "alwaysIncludePattern": true,
-            // 后缀，每小时创建一个新的日志文件        
-            "pattern": "-yyyy-MM-dd-hh.log",
-            // 自定义属性，错误日志的根目录  
-            "path": errorPath
+            "filename": `${baseLogPath}/access.log`,
+            "pattern": "-yyyy-MM-dd",
+            "category": "http"
         },
-        // 响应日志
-        {
+        "app": {
+            "type": "file",
+            "filename": `${baseLogPath}/app.log`,
+            "maxLogSize": 10485760,
+            "numBackups": 3
+        },
+        "out": { type: "stdout" },
+        "server": {
             "type": "dateFile",
-            "category": "resLogger",
-            "filename": responseLogPath,
+            "filename": `${baseLogPath}/server`,
+            "pattern": " [yyyy.MM.dd].log",
             "alwaysIncludePattern": true,
-            "pattern": "-yyyy-MM-dd-hh.log",
-            "path": responsePath
+            "layout": {
+                "type": 'pattern',
+                "pattern": '[%d{yyyy-MM-dd}][%r] - [%p] - %m%n'
+            }
+        },
+        "errors": {
+            "type": "dateFile",
+            "level": "ERROR",
+            "filename": `${baseLogPath}/errors`,
+            "pattern": "  [yyyy.MM.dd].log",
+            "alwaysIncludePattern": true
+        },
+        "console": {
+            "type": "console",
+            "layout": {
+                "type": 'pattern',
+                "pattern": '[ %[%d{yyyy.MM.dd}%] ][ %[%r%] ] [ %[%p%] ] - %m%n'
+            }
         }
-    ],
-    // 设置logger名称对应的的日志等级
-    "levels": {
-        "errorLogger": "ERROR",
-        "resLogger": "ALL"
     },
-    // logs根目录
-    "baseLogPath": baseLogPath
+    "categories": {
+        "default": { "appenders": ["app", "server"], "level": "INFO" },
+        "http": { "appenders": ["access"], "level": "DEBUG" },
+        "[server]": { "appenders": ["app", "server"], "level": "DEBUG" },
+        "[errors]": { "appenders": ["app", "errors"], "level": "ERROR" },
+        "[console]": { "appenders": ["console"], "level": "ALL" }
+    }
 }
--- backend/src/utils/log.util.ts
+++ backend/src/utils/log.util.ts
@@ -6,16 +6,10 @@ import config from "../config/log.config";
 
 export class Logger {
 
-    private errorLogger: any;
-
-    private resLogger: any;
-
     public config: any;
 
     constructor() {
-        let _config = <log4js.Configuration>{};
-        Object.assign(_config, config);
-        this.init(_config);
+        this.init(config);
     }
 
     /**
@@ -25,7 +19,7 @@ export class Logger {
      * @param ms 响应的时间(ms)
      */
     public error(ctx: Context, err: Error, ms: number) {
-        if (ctx && err) { this.errorLogger.error(this.formatErrorLog(ctx, err, ms)); }
+        if (ctx && err) { log4js.getLogger('[errors]').error(this.formatErrorLog(ctx, err, ms)); }
     }
 
     /**
@@ -34,62 +28,84 @@ export class Logger {
      * @param ms 响应的时间(ms)
      */
     public infor(ctx: Context, ms: number) {
-        if (ctx) { this.resLogger.infor(this.formatInforLog(ctx, ms)); }
+        if (ctx) { log4js.getLogger('[server]').info(this.formatInforLog(ctx, ms)); }
     }
 
-    private init(config: log4js.Configuration) {
+    /**
+     * 输出到控制台
+     * @param ctx koa的上下文
+     * @param mes 传达的内容
+     * @param ms 响应的时间(ms)
+     */
+    public console(mes: string) {
+        log4js.getLogger('[console]').info(mes);
+    }
+
+    private async init(config: log4js.Configuration) {
 
         this.config = config;
 
         log4js.configure(config);
 
-        this.errorLogger = log4js.getLogger('errorLogger');
+        // for (let i in config.appenders) {
+        //     if (config.appenders[i]['filename']) {
+        //         if (config.appenders[i]["alwaysIncludePattern"] && config.appenders[i]["pattern"]) {
+        //             let pattern = config.appenders[i]["pattern"];
+        //             pattern.replace(/yyyy/g, new Date().getFullYear());
+        //             pattern.replace(/MM/g, new Date().getMonth() + 1);
+        //             pattern.replace(/dd/g, new Date().getDate());
+        //             await this.check(config.appenders[i]['filename'] + pattern);
+        //         } else {
+        //             await this.check(config.appenders[i]['filename']);
+        //         }
+        //     }
+        // }
+    }
 
-        this.resLogger = log4js.getLogger('resLogger');
-
-        if (config['baseLogPath']) {
-
-            if (!fs.existsSync(config['baseLogPath'])) { fs.mkdirSync(config['baseLogPath']); }
-
-            if (config.appenders) {
-
-                console.dir(config.appenders);
-                for (let i in config.appenders) {
-                }
-            }
+    private check(path: string) {
+        if (!fs.existsSync(path)) {
+            fs.mkdirSync(path);
         }
     }
-    private formatRequest(req: Request, ms: number) {
+
+    private formatRequest(ctx: Context, ms: number) {
 
         let message = new String();
 
+        let req = ctx.request;
+
+        let headers = ctx.headers;
+      
         const method = req.method;
         // 请求方法
-        message += `request method: ${method} \n`;
+        message += `  request method:         ${method}  \n`;
+
+        // 请求地址
+        message += `  request url:            ${ctx.href}  \n`;
 
         // 请求原始地址
-        message += `request originalUrl:  ${req.originalUrl} \n`;
+        message += `  request originalUrl:    ${req.originalUrl}  \n`;
 
         // 客户端ip
-        message += `request client ip:  ${req.ip} \n`;
+        message += `  request client ip:      ${req.ip}  \n`;
 
         switch (method) {
 
             case "GET":
-                message += `request query:  ${JSON.stringify(req.query)} \n`;
+                message += `  request query:          ${JSON.stringify(req.query)}  \n`;
                 break;
 
             case "POST":
-                message += `request body: \n ${JSON.stringify(req.body)} \n`;
+                message += `  request body: \n ${JSON.stringify(req.body)}  \n`;
                 break;
 
             default:
-                message += `request body: \n ${JSON.stringify(req.body)} \n`;
+                message += `  request body: \n ${JSON.stringify(req.body)}  \n`;
                 break;
         }
 
         // 服务器响应时间
-        message += `response time: ${ms} ms \n`;
+        message += `  response time:          ${ms} ms  \n`;
 
         return message.toString();
     }
@@ -102,13 +118,13 @@ export class Logger {
         message += `\n --------------- response log start --------------- \n`;
 
         // 记录响应的内容
-        message += this.formatRequest(ctx.request, ms);
+        message += this.formatRequest(ctx, ms);
 
         //响应状态码
-        message += `response status: ${ctx.status} \n`;
+        message += `  response status:        ${ctx.status}  \n`;
 
         // 响应内容 (PS.个人觉得这个只能在开发调试模式下返回，生产环境应该去掉)
-        message += `response body: \n ${JSON.stringify(ctx.body)} \n`;
+        message += `  response body: \n ${JSON.stringify(ctx.body)}  \n`;
 
         // 结束记录响应日志
         message += `\n ---------------  response log end  --------------- \n`;
@@ -124,16 +140,16 @@ export class Logger {
         message += `\n ===============  error log start   =============== \n`;
 
         // 记录响应的内容
-        message += this.formatRequest(ctx.request, ms);
+        message += this.formatRequest(ctx, ms);
 
         //错误名称
-        message += `err name: err.name \n`;
+        message += `  err name:           ${err.name}  \n`;
 
         //错误信息
-        message += `err message: err.message \n`;
+        message += `  err message:        ${err.message}  \n`;
 
         //错误详情
-        message += `err stack: err.stack \n`;
+        message += `  err stack:          ${err.stack}  \n`;
 
         // 结束记录错误日志
         message += `\n ===============   error log end    =============== \n`;
